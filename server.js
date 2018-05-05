0f8a207d1e2e623ce3eb92ac7ccdba24a53ed27d
Sananda Som <Sean@Sanandas-MacBook-Pro.local>
2018-05-05 13:20:08 -0500

    new shit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let bodyParser = require('body-parser');
 let path = require('path');
 
 let app = express();
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 //set up app to nadle body parsing
 
